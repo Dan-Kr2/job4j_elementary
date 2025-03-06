429c0471ba88dfddba4f2f79fea41a36f10288d5
Dan-Kr2 <x.real.delta@gmail.com>
2025-03-06 17:56:00 +0300

    1.1. Что такое import [#505278]

--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -8,4 +8,10 @@ public class MathFunction {
     public static double multiply(double first, double second) {
         return first * second;
     }
+    public static double minus (double first, double second){
+        return first - second;
+    }
+    public static double del (double first, double second){
+        return first/second;
+    }
 }
