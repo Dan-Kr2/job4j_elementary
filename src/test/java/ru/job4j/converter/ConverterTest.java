c9dca4b31920f8938cf70f88e678f269f4fe1988
Dan-Kr2 <x.real.delta@gmail.com>
2025-03-13 17:03:37 +0300

    1. Модульные тесты [#505277 #535054]

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -13,7 +13,6 @@ class ConverterTest {
         double value = 0.0001f;
         assertThat(output).isEqualTo(expected, withPrecision(value));
     }
-
     @Test
     void whenConvert180RblThen3Dollar() {
         double input = 180;
