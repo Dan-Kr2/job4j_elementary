86937f99ea0de8e7d0ba638fdf1fd3fdb0bce28f
Dan-Kr2 <x.real.delta@gmail.com>
2025-04-11 00:41:54 +0300

    Простое число (оператор прерывания break) [#505214 #537650]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,19 +3,13 @@ package ru.job4j.loop;
 import com.sun.source.tree.BreakTree;
 
 public class CheckPrimeNumber {
-
     public static boolean check(int number) {
-        boolean result = false;
-       if (number == 1) {
-           return result;
-       }
+        boolean result = number > 1;
         for (int i = 2; i < number; i++) {
-
             if (number % i == 0) {
-                return result;
+                result = false;
+                break;
             }
-            }
-        result = true;
-        return result;
+        } return result;
     }
 }
